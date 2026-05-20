01ec4a606f37477c50f77dfcf491f9ce8791c5bf
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-05-20 10:59:22 +0200

    improvement: is_punct utility

--- library/char_utils.hpp
+++ library/char_utils.hpp
@@ -1,6 +1,7 @@
 #ifndef CHAR_UTILS
 #define CHAR_UTILS
 
+#include <cctype>
 const std::size_t ALPHABETS_SIZE = 26;
 
 const std::string LOWER_ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
@@ -18,6 +19,7 @@ inline bool is_alpha(const char ch) { return std::isalpha(static_cast<unsigned c
 inline bool is_lower(const char ch) { return std::islower(static_cast<unsigned char>(ch)); }
 inline bool is_digit(const char ch) { return std::isdigit(static_cast<unsigned char>(ch)); }
 inline bool is_space(const char ch) { return std::isspace(static_cast<unsigned char>(ch)); }
+inline bool is_punct(const char ch) { return std::ispunct(static_cast<unsigned char>(ch)); }
 inline bool is_vowel(const char ch) { return VOWELS.find(to_lower(ch)) != std::string::npos; }
 inline bool is_consonant(const char ch) { return !is_vowel(ch); }
 
